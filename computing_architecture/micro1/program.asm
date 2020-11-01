f80da11f6d818faf50f24169cd16df02da721438
Vakosta <v.akosta@ya.ru>
2020-11-01 18:00:35 +0300

    Add functional for micro1.

--- computing_architecture/micro1/program.asm
+++ computing_architecture/micro1/program.asm
@@ -3,73 +3,112 @@ entry start
 include 'win32a.inc'
 
 section '.data' data readable writable
-        ; Strings for console
-        amount_of_elements db 'The number of elements of the Padovan sequence: %d', 10, 0
-        max_element        db 'The max element is: %d', 0
+        string_extra_output       db 'Want to have extra output?', 10, 0
+        string_yes_no             db '1 -- yes, else -- no: ', 0
+        string_element            db "%d'th element is %d.", 10, 0
+        string_amount_of_elements db 10, 'The number of elements of the Padovan sequence: %d.', 10, 0
+        string_max_element        db 'The max element is: %d.', 0
 
-        counter            dd 2
+        format_number             db '%d', 0
 
-        number1            dd 1
-        number2            dd 1
-        number3            dd 1
+        is_yes                    dd ?
+        counter                   dd 3
+
+        number1                   dd 1
+        number2                   dd 1
+        number3                   dd 1
 
         NULL = 0
 
 
+
 ; ----------------------------------------------------------------
+
 
 
 section '.code' code readable executable
-        start:             call    process
+        start:                    call  input
+                                  call  process
 
-        process:           inc     [counter]
+                                  push  NULL
+                                  call  [ExitProcess]
 
-                           xor     ecx, ecx
-                           add     ecx, [number1]
-                           add     ecx, [number2]
+; ----------------------------------------------------------------
 
-                           mov     ebx, [number2]
-                           mov     [number1], ebx
+        input:                    push  string_extra_output
+                                  call  [printf]
+                                  push  string_yes_no
+                                  call  [printf]
+                                  add   esp, 8
 
-                           mov     ebx, [number3]
-                           mov     [number2], ebx
+                                  push  is_yes
+                                  push  format_number
+                                  call  [scanf]
+                                  add   esp, 8
 
-                           mov     [number3], ecx
+                                  ret
 
-                          ;push    [number3]
-                          ;push    str_elem
-                          ;call    [printf]
+; ----------------------------------------------------------------
 
-                           mov     ebx, [number2]
-                           mov     ecx, [number3]
-                           cmp     ecx, ebx
-                           jge     process
+        process:                  inc   [counter]
 
-                           push    [counter]
-                           push    amount_of_elements
-                           call    [printf]
+                                  xor   ecx, ecx
+                                  add   ecx, [number1]
+                                  add   ecx, [number2]
 
-                           push    [number2]
-                           push    max_element
-                           call    [printf]
+                                  mov   ebx, [number2]
+                                  mov   [number1], ebx
 
-                           call    [getch]
+                                  mov   ebx, [number3]
+                                  mov   [number2], ebx
 
-                           push    NULL
-                           call    [ExitProcess]
+                                  mov   [number3], ecx
+
+                                  mov   ecx, 1
+                                  cmp   [is_yes], ecx
+                                  jne   comparison
+
+                                  push  [number3]
+                                  push  [counter]
+                                  push  string_element
+                                  call  [printf]
+                                  add   esp, 12
+
+        comparison:               mov   ebx, [number2]
+                                  mov   ecx, [number3]
+                                  cmp   ecx, ebx
+                                  jge   process        ; Jump if 1'th more or equal than 2'th argument.
+                                                       ; So if 1'th less then overflow has been occured.
+
+                                  dec   [counter]
+                                  push  [counter]
+                                  push  string_amount_of_elements
+                                  call  [printf]
+                                  add   esp, 8
+
+                                  push  [number2]
+                                  push  string_max_element
+                                  call  [printf]
+                                  add   esp, 8
+
+                                  call  [getch]
+
+                                  ret
+
 
 
 ; ----------------------------------------------------------------
 
 
+
 section '.idata' import data readable
-        library            kernel, 'kernel32.dll',\
-                           msvcrt, 'msvcrt.dll'
+        library            kernel,      'kernel32.dll',\
+                           msvcrt,      'msvcrt.dll'
 
         import             kernel,\
                            ExitProcess, 'ExitProcess'
 
         import             msvcrt,\
-                           printf, 'printf',\
-                           getch, '_getch',\
-                           scanf, 'scanf'
+                           printf,      'printf',\
+                           getch,       '_getch',\
+                           scanf,       'scanf'
